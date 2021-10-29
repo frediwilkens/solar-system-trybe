fca94b4d202ab2a08fa7ba33f012a29f004349e9
Fredi Wilkens <frediwilkens@gmail.com>
2021-10-29 18:15:06 -0300

    feat: renderiza os cards dos planetas

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const cards = planets.map((planet) => (<PlanetCard
+      planetName={ planet.name }
+      planetImage={ planet.image }
+      key={ planet.name }
+    />));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {cards}
       </div>
     );
   }
